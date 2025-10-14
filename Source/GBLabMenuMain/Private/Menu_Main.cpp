c10692178a868b6cd6bed4effe959f18b206ef4d
GetBoros <getboros@gmail.com>
2025-10-14 19:49:33 +0300

    feat: Add cpp code, refact: Add to plugin content UIMaterialLab

--- Source/GBLabMenuMain/Private/Menu_Main.cpp
+++ Source/GBLabMenuMain/Private/Menu_Main.cpp
@@ -1,6 +1,12 @@
 //------------------------------------------------------------------------------------------------------------
 #include "Menu_Main.h"
 //------------------------------------------------------------------------------------------------------------
+#include "Materials/MaterialInstanceDynamic.h" // <-- ÃËÀÂÍÛÉ ÈÍÊËÞÄ!
+
+#include "Components/Image.h"
+#include "Components/TextBlock.h"
+#include "Materials/MaterialInterface.h"
+//------------------------------------------------------------------------------------------------------------
 
 
 
@@ -12,9 +18,19 @@ void UGBLab_Menu_Main_Button::NativePreConstruct()
 
 }
 //------------------------------------------------------------------------------------------------------------
-void UGBLab_Menu_Main_Button::Init_Widget()
+void UGBLab_Menu_Main_Button::Init_Widget(UTextBlock *text, UImage *image)
 {
+	Image_Button = image;
+	TB_Button = text;
+}
+//------------------------------------------------------------------------------------------------------------
+void UGBLab_Menu_Main_Button::Update()
+{
+	DMI_Button_Background = UMaterialInstanceDynamic::Create(Button_Background, this);
+	DMI_Button_Background->SetScalarParameterValue("Hovered", 0.0f);
 
+	Image_Button->SetBrushFromMaterial(DMI_Button_Background);
+	TB_Button->SetText(Button_Text);
 }
 //------------------------------------------------------------------------------------------------------------
 
--- Source/GBLabMenuMain/Public/Menu_Main.h
+++ Source/GBLabMenuMain/Public/Menu_Main.h
@@ -5,6 +5,9 @@
 //------------------------------------------------------------------------------------------------------------
 #include "Menu_Main.generated.h"
 //------------------------------------------------------------------------------------------------------------
+class UTextBlock;
+class UImage;
+//------------------------------------------------------------------------------------------------------------
 UCLASS(meta = (DisableNativeTick) ) class UGBLab_Menu_Main_Button : public UUserWidget
 {
 	GENERATED_BODY()
@@ -12,12 +15,15 @@ UCLASS(meta = (DisableNativeTick) ) class UGBLab_Menu_Main_Button : public UUser
 public:
 	virtual void NativePreConstruct();
 
-	UFUNCTION(BlueprintCallable) void Init_Widget();
+	UFUNCTION(BlueprintCallable) void Init_Widget(UTextBlock *text, UImage *image);
+	UFUNCTION(BlueprintCallable) void Update();
 
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite) UImage *Image_Button = 0;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite) UTextBlock *TB_Button = 0;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (ExposeOnSpawn = "true", ToolTip = "temp") ) FText Button_Text;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (ExposeOnSpawn = "true", ToolTip = "temp") ) UMaterialInterface *Button_Background = 0;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) UMaterialInstanceDynamic *DMI_Button_Background = 0;
 };
-//------------------------------------------------------------------------------------------------------------
-class UTextBlock;
-class UImage;
 //------------------------------------------------------------------------------------------------------------
 UCLASS(meta = (DisableNativeTick) ) class GBLABMENUMAIN_API UGBLab_Menu_Main: public UUserWidget
 {
@@ -30,6 +36,5 @@ public:
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite) UImage *Image_Temp = 0;
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite) UTextBlock *Text_Block = 0;
-
 };
 //------------------------------------------------------------------------------------------------------------
